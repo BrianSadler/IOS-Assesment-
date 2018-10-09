6a53911d10a81c5d34898df19f873542fd08892a
Brian Sadler <briansadler@MacBook-Pro-3.local>
2018-10-09 09:49:41 -0500

    Initial Commit

---
+++ IOS Assesment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS Assesment
+//
+//  Created by Brian Sadler on 10/9/18.
+//  Copyright © 2018 Brian Sadler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
